c5c17acbca7a97382db1dd293d02501786c6e895
Mariusz Dziecielski <mariuszdziec@gmail.com>
2018-07-01 14:01:48 +0200

    Minor fixes to pass end-to-end tests via Protractor & add a new favicon

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to star-trek-battle!');
+    expect(page.getParagraphText()).toEqual('Witaj w grze KlingonRomulaninKardasjanin');
   });
 });
